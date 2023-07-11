3a71c5bc76b7af8d9e0be2677a060fbca3548ac0
RobertoMaligni <samuele.carradori@stud.unifi.it>
2023-07-11 17:43:19 +0200

    Unit testing

--- test/runListoneTests.cpp
+++ test/runListoneTests.cpp
@@ -1,6 +1,17 @@
 #include "gtest/gtest.h"
 #include "../Listone.h"
 
-TEST(Listone,InstantiationFromFile){
+TEST(Listone,Instantiation){
     //TODO
+    Listone* listone = Listone::getInstance();
+    ASSERT_TRUE(listone->isRunning());
+}
+
+TEST(Listone, LoginUser){
+    Listone* listone = Listone::getInstance();
+    listone->loginUser("","");
+    listone->loginUser("FrancescoTotti","password"); //correct
+    listone->loginUser("FrancescoTotti","passwordsbagliata");
+    listone->loginUser("FrancescoTotti","");
+    listone->loginUser("","password");
 }
